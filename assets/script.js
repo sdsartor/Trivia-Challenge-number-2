090deffeb1cdfe2fd8a4a2edc931a8d20fc45d10
sdsartor <sdsartor48@gmail.com>
2023-01-18 11:57:11 -0800

    touch up on code to get most questjions to pop up through the javascript files

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
-const questionContainerElement =document.getElementById('question-container')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
@@ -14,58 +14,54 @@ nextButton.addEventListener('click', () => {
   setNextQuestion()
 })
 
-  function startGame(){
-console.log('Started')
-startButton.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
-
-  }
-  
-  function setNextQuestion() {
-resetstate()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-  
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-  function showQuestion(question) {
-questionElement.innerText = question.question
-question.answers.forEach(answer => {
-  const button = document.createElement('button')
-  button.innerText = answer.text
-  button.classList.add('btn')
-  if (answer.correct) {
-    button.dataset.correct = answer.correct
-  }
-  button.addEventListener('click', selectAnswer)
-  answerButtonsElement.appendChild(button)
-})
-  }
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-function resetstate() {
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
   clearStatusClass(document.body)
   nextButton.classList.add('hide')
-  while(answerButtonsElement.firstChild) {
+  while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
 }
 
-function selectAnswer(e) {
-const selectedButton = e.target
-const correct = selectedButton.dataset.correct
-setStatusClass(document.body, correct)
-Array.from(answerButtonsElement.children).forEach(button => {
-  setStatusClass(button, button.dataset.correct)
-})
-}
-console.log(question.length)
-if (shuffledQuestions.length > currentQuestionIndex + 1) {
-nextButton.classList.remove('hide')
-} else {
-  startButton.innerText = 'Restart'
-  startButton.classList.remove('hide')
+function selectAnswer(l) {
+  const selectedButton = l.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
 }
 
 function setStatusClass(element, correct) {
@@ -73,14 +69,15 @@ function setStatusClass(element, correct) {
   if (correct) {
     element.classList.add('correct')
   } else {
-     element.classList.add('wrong')
+    element.classList.add('wrong')
   }
-  }
+}
+
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
-
 }
+
 
 const questions = [
   {
@@ -129,5 +126,5 @@ const questions = [
     { text: 'for loops', correct: false},
     { text: 'console.log', correct: true}
     ]
-  },
-];
+  }
+]
