923d1527a49385645e58b652afd2fac125db260b
sdsartor <sdsartor48@gmail.com>
2023-01-18 20:04:55 -0800

    finalized timer to enhance the stress on the quiz taker

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,28 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      seconds = parseInt(timer % 60, 10);
+
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var tenSeconds = 60 * 10,
+      display = document.querySelector('#time');
+  startTimer(tenSeconds, display);
+};
+
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
